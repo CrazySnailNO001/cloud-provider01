3e40d86e30a56376bab4594d547e2008a2a915d0
xzh <xzh_xuzhonghui@163.com>
2019-04-29 11:16:30 +0800

    init

---
+++ src/main/java/com/xzh/provider/controller/HelloController.java
@@ -0,0 +1,23 @@
+package com.xzh.provider.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author XZHH
+ * @Description:
+ * @create 2019/4/23 0023 12:03
+ * @modify By:
+ **/
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "这是服务提供者1，参数："+name;
+    }
+
+    @PostMapping("/hello")
+    @ResponseBody
+    public String ribbonPost(@RequestBody String name) {
+        return "这是服务提供者1，post请求,参数：" + name;
+    }
+}
