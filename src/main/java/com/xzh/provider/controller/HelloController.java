ee74bec458b02860abb646c72351d1e6cdc6c956
Zhonghui XU <zhonghui.xu.partner@decathlon.com>
2020-08-03 16:16:09 +0800

    modify config

--- src/main/java/com/xzh/provider/controller/HelloController.java
+++ src/main/java/com/xzh/provider/controller/HelloController.java
@@ -12,30 +12,25 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @Slf4j
 public class HelloController {
-    @RequestMapping("/hello")
-    public String index(@RequestParam String name) throws InterruptedException {
-        Thread.sleep(60);
+    @GetMapping("/hello")
+    public String hello(@RequestParam(required = false) String name) throws InterruptedException {
 
-        String response = "这是provider-service1，参数：" + name;
+        Thread.sleep(60);
+        String response = "[provider-service1 get hello API] ,RequestParam ：" + name;
         log.info("[Hello World API] response : {}", response);
 
         return response;
     }
 
-    @PostMapping("/hello")
-    @ResponseBody
-    public String ribbonPost(@RequestBody String name) {
-        return "这是服务提供者1，post请求,参数：" + name;
+    @GetMapping("/hystrix_timeout")
+    public String hystrixTimeOut(@RequestParam long time) throws InterruptedException {
+        log.info("[Provider-001 hystrixTimeOut API] get requestParam : [ {} ]", time);
+        Thread.sleep(time);
+        return "success";
     }
 
-
-    @RequestMapping("/timeout")
-    public String timeout(@RequestParam String name) throws InterruptedException {
-        Thread.sleep(60);
-
-        String response = "这是provider-service1，参数：" + name;
-        log.info("[Timeout API] response : {}", response);
-
-        return response;
+    @PostMapping("/hello")
+    public String ribbonPost(@RequestBody String name) {
+        return "这是服务提供者1，post请求,参数：" + name;
     }
 }
